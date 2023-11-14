35cc17e9650d39304fe884083090aabf10d525e4
DITGO <citpcgo@gmail.com>
2023-11-14 17:33:42 -0300

    Implementação de estratégias.

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,20 @@ import { time } from "console";
 function App() {
   const symbols = [
     { value: "", label: "Escolha uma moeda" },
-    { value: "BTCUSDT", label: "BTCUSDT" },
+    { value: "BTCUSDT", label: "BitCoin" },
+    { value: "ETHUSDT", label: "Ethereum" },
+    { value: "BNBUSDT", label: "Binance" },
+    { value: "INJUSDT", label: "Injective" },
+  ];
+  const strategyCombo = [
+    { value: "", label: "Escolha um estrategia" },
+    { value: "PRICE", label: "Preço definido" },
+    { value: "SMA", label: "Média Móvel" },
+    { value: "EMA", label: "Exponencial Móvel" },
   ];
   const [trade, setTrade] = useState([]);
   const [symbol, setSymbol] = useState("");
+  const [strategy, setStrategy] = useState("");
   const [buyPrice, setBuyPrice] = useState("0.0");
   const [sellPrice, setSellPrice] = useState("0.0");
   const [priceCurrent, selPriceCurrent] = useState("0.00");
@@ -45,73 +55,107 @@ function App() {
       const sma: number = calcSMA(data);
       const timestamp = Date.now();
       const time = new Date(timestamp).toLocaleString("pt-BR");
-      const priceBuy = parseFloat((sma * 0.95).toString());
-      const priceSell = parseFloat((sma * 1.05).toString());
+      const priceBuy = parseFloat((sma * 0.98).toString());
+      const priceSell = parseFloat((sma * 1.02).toString());
 
-      //if (price <= priceBuy) alert("Bora comprar meu povo.");
-      //if (price >= priceBuy) alert("Bora vender meu povo.");
+      console.log(
+        "Valor Moeda: " +
+          price +
+          " | Valor Buy: " +
+          priceBuy +
+          " | Valor Sell: " +
+          priceSell
+      );
 
-      if (price < priceBuy && !isOpened) {
-        /*if (
-          window.confirm("Apareceu uma oportunidade de compra, posso comprar?")
-        ) {*/
-        //faça a ordem
-        const order = newOrder("BUY");
-        console.log(price + "<" + priceBuy);
-        status = {
-          order: logTrade.length + 1,
-          status: `Compra Efetuada`,
-          valor: `$${price.toFixed(4)}`,
-          sma: `$${(sma * 0.95).toFixed(4)}`,
-          time: `${time}`,
-        };
-        //isOpened para true
-        setIsOpened(true);
-        /*} else {
+      if (strategy === "PRICE") {
+        if (price <= parseFloat(buyPrice) && !isOpened) {
+          //faça a ordem
+          const order = newOrder("BUY");
           status = {
             order: logTrade.length + 1,
-            status: `Opção de Compra Negada`,
+            status: `Compra Efetuada`,
             valor: `$${price.toFixed(4)}`,
             sma: `$${(sma * 0.95).toFixed(4)}`,
             time: `${time}`,
+            direction: "BUY",
           };
-        }*/
-      } else if (price > priceSell && isOpened) {
-        /*if (
-          window.confirm("Apareceu uma oportunidade de venda, posso vender?")
-        ) {*/
-        //faça a ordem
-        console.log(price + ">" + priceBuy);
-        const order = newOrder("SELL");
-        status = {
-          order: logTrade.length + 1,
-          status: `Venda Efetuada`,
-          valor: `$${price.toFixed(4)}`,
-          sma: `$${(sma * 1.05).toFixed(4)}`,
-          time: `${time}`,
-        };
-        //isOpened para true
-        setIsOpened(false);
-        /*} else {
+          //isOpened para true
+          setIsOpened(true);
+        } else if (price >= parseFloat(sellPrice) && isOpened) {
+          //faça a ordem
+          const order = newOrder("SELL");
           status = {
             order: logTrade.length + 1,
-            status: `Opção de Venda Cancelada`,
+            status: `Venda Efetuada`,
             valor: `$${price.toFixed(4)}`,
             sma: `$${(sma * 1.05).toFixed(4)}`,
             time: `${time}`,
+            direction: "SELL",
           };
-        }*/
+          //isOpened para true
+          setIsOpened(false);
+        } else {
+          console.log(price + " > " + priceBuy + " / Opened: " + isOpened);
+          console.log(price + " > " + priceSell + " / Opened: " + isOpened);
+          const situacao = isOpened ? "Comprado" : "";
+          status = {
+            order: logTrade.length + 1,
+            status: `Aguardando`,
+            valor: `$${price.toFixed(4)}`,
+            sma: `$${sma.toFixed(4)}`,
+            time: `${time}`,
+            direction: "AWAIT",
+          };
+        }
+      } else if (strategy === "SMA") {
+        if (price <= priceBuy) {
+          if (isOpened === false) {
+            console.log(`${time} - Compra realizada`);
+            //faça a ordem
+            const order = newOrder("BUY");
+            status = {
+              order: logTrade.length + 1,
+              status: `Compra Efetuada`,
+              valor: `$${price.toFixed(4)}`,
+              sma: `$${(sma * 0.95).toFixed(4)}`,
+              time: `${time}`,
+              direction: "BUY",
+            };
+            //isOpened para true
+            setIsOpened(true);
+          }
+        } else if (price >= priceSell) {
+          if (isOpened == true) {
+            //faça a ordem
+            const order = newOrder("SELL");
+            status = {
+              order: logTrade.length + 1,
+              status: `Venda Efetuada`,
+              valor: `$${price.toFixed(4)}`,
+              sma: `$${(sma * 1.05).toFixed(4)}`,
+              time: `${time}`,
+              direction: "SELL",
+            };
+            //isOpened para true
+            setIsOpened(false);
+          }
+        } else {
+          console.log(price + " > " + priceBuy + " / Opened: " + isOpened);
+          console.log(price + " > " + priceSell + " / Opened: " + isOpened);
+          const situacao = isOpened ? "Comprado" : "";
+          status = {
+            order: logTrade.length + 1,
+            status: `Aguardando`,
+            valor: `$${price.toFixed(4)}`,
+            sma: `$${sma.toFixed(4)}`,
+            time: `${time}`,
+            direction: "AWAIT",
+          };
+        }
       } else {
-        console.log(price + ">" + priceBuy + " / Opened" + isOpened);
-        const situacao = isOpened ? "Venda" : "Compra";
-        status = {
-          order: logTrade.length + 1,
-          status: `Aguardando ${situacao}`,
-          valor: `$${price.toFixed(4)}`,
-          sma: `$${sma.toFixed(4)}`,
-          time: `${time}`,
-        };
+        alert("Não identifiquei a sua estratégia!");
       }
+
       logTrade.unshift(status);
       setTrade(logTrade);
       const interval = setTimeout(startTrade, parseFloat(timerRequest));
@@ -181,12 +225,24 @@ function App() {
       </header>
       <main style={{ width: "99vw" }}>
         <div className='container'>
-          <div style={{ width: "100%", height: "40px" }}>
-            <div>Trade</div>
+          <div style={{ width: "99%", height: "40px" }}>
+            <div>
+              <h2>Trade</h2>
+            </div>
           </div>
-          <hr></hr>
+          <hr style={{ width: "90%" }}></hr>
           <div style={{ display: "flex" }}>
-            <div style={{ width: "45vw" }}>
+            <div
+              style={{
+                width: "20vw",
+                borderRight: "1px dashed #000",
+                display: "flex",
+                flexDirection: "column",
+                padding: 10,
+                height: "95%",
+              }}
+            >
+              <h3>Parâmetro para o Trade</h3>
               <div
                 style={{
                   width: "10vw",
@@ -198,6 +254,26 @@ function App() {
                 <select onChange={(e) => setSymbol(e.target.value)}>
                   {symbols.map((symbol) => {
                     return <option value={symbol.value}>{symbol.label}</option>;
+                  })}
+                </select>
+                <br />
+                {priceCurrent != "0.00"
+                  ? `Preço atual é : $${priceCurrent}`
+                  : "Selecione uma moeda!"}
+              </div>
+              <div
+                style={{
+                  width: "10vw",
+                  borderBottom: "1px dashed silver",
+                  padding: 5,
+                }}
+              >
+                <label>Estratégia</label>
+                <select onChange={(e) => setStrategy(e.target.value)}>
+                  {strategyCombo.map((strategy) => {
+                    return (
+                      <option value={strategy.value}>{strategy.label}</option>
+                    );
                   })}
                 </select>
                 <br />
@@ -311,17 +387,32 @@ function App() {
                 <button onClick={(e) => stopTrade()}>Stop Trade</button>
               </div>
             </div>
-            <div style={{ width: "45vw" }}>
+            <div
+              style={{
+                width: "20vw",
+                display: "flex",
+                flexDirection: "column",
+                padding: 10,
+                height: "95%",
+                borderRight: "1px dashed #000",
+              }}
+            >
+              <h3>Ordem executada</h3>
+            </div>
+            <div
+              style={{
+                width: "55vw",
+                padding: 10,
+              }}
+            >
               <div style={{ width: "99%" }}>
-                <div
-                  style={{ width: "99%", textAlign: "center", padding: 2.5 }}
-                >
+                <div style={{ width: "99%", textAlign: "center" }}>
                   <h3>Log Trade</h3>
                 </div>
                 <div
                   style={{
-                    width: "99%",
-                    height: "75vh",
+                    width: "95%",
+                    height: "70vh",
                     textAlign: "center",
                     backgroundColor: "#f0f0f0",
                     borderRadius: 5,
@@ -331,7 +422,7 @@ function App() {
                 >
                   <div
                     style={{
-                      width: "95%",
+                      width: "98%",
                       padding: 5,
                       borderBottom: "1px dashed silver",
                       display: "flex",
@@ -347,10 +438,10 @@ function App() {
                   </div>
                   <div
                     style={{
-                      width: "97%",
+                      width: "99%",
                       display: "grid",
                       overflowY: "scroll",
-                      height: "95%",
+                      height: "90%",
                     }}
                   >
                     {trade.map((item: any) => {
@@ -362,7 +453,11 @@ function App() {
                             border: "1px dashed silver",
                             display: "flex",
                             backgroundColor:
-                              item.valor < item.sma ? "#8DFF80" : "#FF9780",
+                              item.direction === "AWAIT"
+                                ? "#F9FF80"
+                                : item.direction == "BUY"
+                                ? "#8DFF80"
+                                : "#FF9780",
                           }}
                         >
                           <div style={{ width: "5%" }}>{item.order}</div>
